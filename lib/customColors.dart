73c8ed67e71ea1e40f9e79a99cc4c2d9b9546b8b
Vedat <cetintasvedat@gmail.com>
2022-03-15 23:19:07 +0300

    custom colors added

--- lib/customColors.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-class CustomColor{
-  static final Color green = const Color.fromRGBO(123, 237, 141,1);
-  static final Color inputBackColor = const Color(0xfff0f4f8);
-}
---
+++ lib/customStyle/customColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static final Color green = const Color.fromRGBO(123, 237, 141, 1);
+  static final Color inputBackColor = const Color(0xfff0f4f8);
+  static final Color greyTextColor = const Color(0xffcccdd0);
+}
