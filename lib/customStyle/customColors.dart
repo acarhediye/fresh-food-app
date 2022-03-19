d196f15c781490ca340d823272b0e87a798d2ebd
Vedat <cetintasvedat@gmail.com>
2022-03-20 02:45:50 +0300

    colors updated

--- lib/customStyle/customColors.dart
+++ lib/customStyle/customColors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomColor {
-  static final Color green = const Color.fromRGBO(123, 237, 141, 1);
-  static final Color inputBackColor = const Color(0xfff0f4f8);
-  static final Color greyTextColor = const Color(0xffcccdd0);
+  static const lightGreen = Color(0xff7bed8d);
+  static const greyBlue = Color(0xff748a9d);
+  static const paleGrey = Color(0xfff0f4f8);
 }
