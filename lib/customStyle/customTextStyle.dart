867ace58b55acd0e9299dc793a41c5e89afaae29
Vedat <cetintasvedat@gmail.com>
2022-03-27 21:19:02 +0300

    Text Style added

--- lib/customStyle/customTextStyle.dart
+++ lib/customStyle/customTextStyle.dart
@@ -2,16 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:fresh_food/customStyle/customColors.dart';
 
 class CustomTextStyle {
-  static final TextStyle buttonTextStyle =
-      TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontFamily: "AcuminPro", fontStyle: FontStyle.normal, fontSize: 18);
+  static final TextStyle buttonTextStyle = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w700,
+      fontFamily: "AcuminPro",
+      fontStyle: FontStyle.normal,
+      fontSize: 18);
 
-  static final TextStyle greyContentStyle =
-      TextStyle(color: CustomColor.greyBlue, fontWeight: FontWeight.w500, fontFamily: "AcuminPro", fontStyle: FontStyle.normal, fontSize: 18);
+  static final TextStyle greyContentStyle = TextStyle(
+      color: CustomColor.greyBlue,
+      fontWeight: FontWeight.w500,
+      fontFamily: "AcuminPro",
+      fontStyle: FontStyle.normal,
+      fontSize: 18);
+  static final TextStyle lightGreyContentStyle = TextStyle(
+      color: CustomColor.lightGreyBlue,
+      fontWeight: FontWeight.w500,
+      fontFamily: "AcuminPro",
+      fontStyle: FontStyle.normal,
+      fontSize: 18);
+  static final TextStyle blackContentStyle = TextStyle(
+      color: CustomColor.greyBlue,
+      fontWeight: FontWeight.w500,
+      fontFamily: "AcuminPro",
+      fontStyle: FontStyle.normal,
+      fontSize: 18);
 
-  static final TextStyle greyBoldContentStyle =
-      TextStyle(color: CustomColor.greyBlue, fontWeight: FontWeight.bold, fontFamily: "AcuminPro", fontStyle: FontStyle.normal, fontSize: 18);
+  static final TextStyle greyBoldContentStyle = TextStyle(
+      color: CustomColor.greyBlue,
+      fontWeight: FontWeight.bold,
+      fontFamily: "AcuminPro",
+      fontStyle: FontStyle.normal,
+      fontSize: 18);
   static final TextStyle favoriesTextButtonStyle = TextStyle(
-      color: CustomColor.favoriesTextButtonColor, fontWeight: FontWeight.w500, fontFamily: "AcuminPro", fontStyle: FontStyle.normal, fontSize: 10.7);
+      color: CustomColor.favoriesTextButtonColor,
+      fontWeight: FontWeight.w500,
+      fontFamily: "AcuminPro",
+      fontStyle: FontStyle.normal,
+      fontSize: 10.7);
 
   static final TextStyle greyHeaderStyle = TextStyle(
       color: CustomColor.greyBlue,
